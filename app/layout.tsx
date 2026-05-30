af1fc1fc95f6e8d000fc020739b736988eb14ee2
Philippe Davet <philippe@blondeau.local>
2026-05-30 10:36:39 +0200

    fix: disable SSG

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: 'ArtMuse',
   description: 'Blondeau - suivi des propositions',
